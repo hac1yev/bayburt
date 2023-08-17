a5fe8d16362e8a1af2eff768db838a79f8e9b53b
elvin <cabbarli.elvin75@gmail.com>
2023-08-17 11:51:46 +0400

    footer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Header from './components/Header/Header';
 import { Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <>
       <Header />
       <Routes>
-
+          <Route path='/' element={<Home />} />
       </Routes>
       <Footer />
     </>
