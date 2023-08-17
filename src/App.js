dffa549e11bfdc6f785c97a9d80c6065ea0c5134
hac1yev <ilkinhaciyev955@gmail.com>
2023-08-17 12:21:56 +0400

    aa

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import { Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 
 const App = () => {
+  const mode = window.localStorage.getItem('mode')
+
   return (
-    <>
+    <div className=''>
       <Header />
       <Routes>
 
       </Routes>
       <Footer />
-    </>
+    </div>
   );
 };
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import senbay_logo from '../../assets/images/senbay_logo.svg';
 import { FiSun } from 'react-icons/fi';
@@ -6,24 +6,44 @@ import { BsMoon } from 'react-icons/bs';
 import { GiHamburgerMenu } from 'react-icons/gi';
 
 const Header = () => {
-  return (
-    <header>
-        <div className='header-logo'>
-            <img src={senbay_logo} alt="senbay-logo" />
-        </div>
-        <div className='dark-light-wrap'>
-            <div className='light-icon'>
-                <FiSun />
+    const [isDarkMode,setIsDarkMode] = useState(false);
+
+    const handleDarkMode = () => {
+        if(isDarkMode){
+            return;
+        }else{
+            setIsDarkMode(true);
+            window.localStorage.setItem('DARK', true);
+        }
+    };
+
+    const handleLightMode = () => {
+        if(!isDarkMode){
+            return;
+        }else{
+            setIsDarkMode(false);
+            window.localStorage.setItem('DARK', false);
+        }
+    };
+
+    return (
+        <header>
+            <div className='header-logo'>
+                <img src={senbay_logo} alt="senbay-logo" />
             </div>
-            <div className='dark-icon'>
-                <BsMoon />
+            <div className='dark-light-wrap'>
+                <div onClick={handleLightMode} className={isDarkMode ? 'light-icon' : 'light-icon mode-active'}>
+                    <FiSun />
+                </div>
+                <div onClick={handleDarkMode} className={isDarkMode ? 'dark-icon mode-active' : 'dark-icon'}>
+                    <BsMoon />
+                </div>
             </div>
-        </div>
-        <div className='menu-icon'>
-            <GiHamburgerMenu />
-        </div>
-    </header>
-  );
+            <div className='menu-icon'>
+                <GiHamburgerMenu />
+            </div>
+        </header>
+    );
 };
 
 export default Header;
