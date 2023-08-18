2bb2e7889764c7f1862e99ed6def1d71dc68d659
hac1yev <ilkinhaciyev955@gmail.com>
2023-08-18 09:12:06 +0400

    gdg

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import './Header.css';
 import senbay_logo from '../../assets/images/senbay_logo.svg';
 import { FiSun } from 'react-icons/fi';
@@ -6,6 +5,7 @@ import { BsMoon } from 'react-icons/bs';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { useDispatch, useSelector } from 'react-redux';
 import { modeSliceActions } from '../../store/mode-slice';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     const isDarkMode = useSelector(state => state.modeReducer.mode);
@@ -34,8 +34,52 @@ const Header = () => {
                     <BsMoon />
                 </div>
             </div>
-            <div className='menu-icon'>
-                <GiHamburgerMenu />
+            <div className='open-menu-link'>
+                <ul>
+                    <li>
+                        <Link to="/">
+                            ƏSAS SƏHİFƏ
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/">
+                            HAQQIMIZDA
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/">
+                            FƏALİYYƏT SAHƏLƏRİ
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/">
+                            LAYİHƏLƏR
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/">
+                            SOSİAL MƏSULİYYƏT
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/">
+                            XƏBƏRLƏR
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/">
+                            ƏLAQƏ
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/">
+                            AZ
+                        </Link>
+                    </li>
+                </ul>
+                <div className='menu-icon'>
+                    <GiHamburgerMenu />
+                </div>
             </div>
         </header>
     );
