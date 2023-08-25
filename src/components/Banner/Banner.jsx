6ede6096ce0cd43775efcb03bbf353c75789f7cc
hac1yev <ilkinhaciyev955@gmail.com>
2023-08-25 16:17:58 +0400

    history page

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import banner from '../../assets/images/banner.svg';
+import './Banner.css';
+
+const Banner = () => {
+    return (
+        <div className='banner-container'>
+            <div>
+                <h1>TARİXÇƏ</h1>
+                <p>HAQQIMIZDA/TARİXÇƏ</p>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
