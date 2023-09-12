7bf417661e329f16fe95237cf1fe405174ef66ec
SemaAbdullayeva <semaabdullayeva02@gmail.com>
2023-09-12 16:20:40 +0400

    insaat sehifesi

---
+++ src/assets/dummyApi/ConstructorApi.js
@@ -0,0 +1,40 @@
+import logo1dark from '../images/construction/logo1.svg';
+import logo2dark from '../images/construction/logo2.svg';
+import logo3dark from '../images/construction/logo3.svg';
+import logo1 from '../images/construction/logo1 light.svg';
+import logo2 from '../images/construction/logo2 light.svg';
+import logo3 from '../images/construction/logo3 light.svg';
+import img1 from '../images/construction/bayburt-grup-image light.svg';
+import img2 from '../images/construction/bayburt-insaat-gorsel light.svg';
+import img3 from '../images/construction/ozgun-yapi-gorsel light.svg';
+import img1dark from '../images/construction/bayburt-grup-image 1 1.svg';
+import img2dark from '../images/construction/bayburt-insaat-gorsel 1 1.svg';
+import img3dark from '../images/construction/ozgun-yapi-gorsel 1 1.svg';
+
+
+export const constructorApi = [
+    {
+        id: 'c1',
+        title: 'BAYBURT GRUP AŞ',
+        description:'Bayburt Grup AŞ Podratçılıq, yanacaq satışı və istismarı, siğorta və vasitəçilik xidmətləri, məsləhətçilik, daxili və xarici ticarət, neft və neft məhsullarının paylanılması, yaş tərəvəz və meyvə istehsalı kimi müxtəlif çeşidli öhdəliklərini vaxtından əvvəl yerinə yetirmək, böyük layihələrə imza atmaq, ölkə iqtisadiyyatına fayda vermək, ixtisaslı işçilər hazırlamaq və işlə təmin etmə cəhdləri bugünki kimi sabah da yeni sektör, seni şirkət fəaliyyətləriylə davam edəcəkdir. Ölkə daxilindəki məlumat və təcrübəsini ölkə xaricinə daşıyan, daxildə olduğu kimi xaricdə də böyük layihə və işlərə imza atacaq olan qrup şirkətlərimiz gördükləri iş və xidmətin keyfiyyətinə görə adlarından bəhs etdirəcəklər.',
+        logo:[logo1, logo1dark],
+        img:[img1, img1dark],
+
+    },
+    {
+        id: 'c2',
+        title: 'BAYBURT İNŞAAT',
+        description:'Bayburt İnşaat quruluşundan bəri Kənd Xidmeti Ümumi Müdirliyi, ASKİ Ümumi Müdirliyi, DSİ Ümumi Müdirliyi, Avtomobil yolları Ümumi Müdirliyi tərkibində tender edilən və Dünya Bankı tərəfindən maliyyələşdirilən, bənd, yol, körpü, suvarma və içməli su, drenaj və kanalizasiya tikintisi, torpaq konsolidasiyası və düzəldilməsi kimi bütün infrastruktur işlərini müvəffəqiyyətlə etmiş və etməyə də davam etməkdədir. Bu öhdəliklərin yerinə yetirilməsində yüzdən çox iş maşını, vasitə və vəsait parkına sahib şirkət hər keçən gün ehtiyacları istiqamətində maşın, vasitə və vəsait parkını yeniləyərək öhdəliklərini vaxtında tamamlayır. Kifayət qədər maşın, nəqliyyat vasitəsi və nəqliyyat parkı, texniki və profesional işçi qüvvəsi ilə bugün gəlinən nöqtədə sektorunun iddialı firmalarından biri olma qüruruna sahibdir.',
+        logo:[logo2, logo2dark],
+        img:[img2, img2dark],
+
+    },
+    {
+        id: 'c3',
+        title: 'ÖZGÜN YAPI',
+        description:'Firma ixtisas sahəsi dəmiryolu, liman, mənzil, mənzil infrastrukturu, ətraf mühitin yaşıllaşdırılması, sahə quraşdırılması, şəhər kanalzasiyası, içmə suyu, gölməçə, drenaj, Mədəniyyət Nazirliyi nəzdində Köhnə Əsərlərin bərpası( incə qələm işləri, əl bəzəkləri, dam təmiri, daş təmizləmə işləri vs.) və yeraltı infrastruktur işləri ilə fəaliyyət sahələrini genişləndirmişdir.',
+        logo:[logo3, logo3dark],
+        img:[img3, img3dark],
+
+    }
+]
