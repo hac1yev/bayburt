fe94bb1bdb9c49c744701ddbb566a0736e23290e
SemaAbdullayeva <semaabdullayeva02@gmail.com>
2023-09-19 15:39:54 +0400

    Company Group sehifesi

--- src/assets/dummyApi/companygroup.js
+++ src/assets/dummyApi/companygroup.js
@@ -1,17 +1,63 @@
-export const images1 = [
-    require('../../assets/images/companygroup/logo 3.svg'),
+export const companyGroupImgs = [
+    {
+        img1: require('../../assets/images/companygroup/bayburt insaat light.svg'),
+        img2: require('../../assets/images/companygroup/bayburt insaat dark.svg'),
+        img3: require('../../assets/images/companygroup/bayburt insaat light hover.svg'),
+        img4: require('../../assets/images/companygroup/bayburt insaat dark hover.svg'),
+    },
 
-];
-
-
-export const images2 = [
-
-];
-
-export const images3 = [
-
-]
-
-export const images4 = [
-
+    {
+        img1: require('../../assets/images/companygroup/bayburt enerji light.svg'),
+        img2: require('../../assets/images/companygroup/bayburt enerji dark.svg'),
+        img3: require('../../assets/images/companygroup/bayburt enerji light hover.svg'),
+        img4: require('../../assets/images/companygroup/bayburt enerji dark hover.svg'),
+    },
+    {
+        img1: require('../../assets/images/companygroup/bayburtbeton light.svg'),
+        img2: require('../../assets/images/companygroup/bayburtbeton dark.svg'),
+        img3: require('../../assets/images/companygroup/bayburt beton light hover.svg'),
+        img4: require('../../assets/images/companygroup/bayburt beton dark hover.svg'),
+    },
+    {
+        img1: require('../../assets/images/companygroup/ozgun light.svg'),
+        img2: require('../../assets/images/companygroup/ozgun dark.svg'),
+        img3: require('../../assets/images/companygroup/ozgun light hover.svg'),
+        img4: require('../../assets/images/companygroup/ozgun dark hover.svg'),
+    },
+    {
+        img1: require('../../assets/images/companygroup/senbayp light.svg'),
+        img2: require('../../assets/images/companygroup/senbayp dark.svg'),
+        img3: require('../../assets/images/companygroup/senbayp light hover.svg'),
+        img4: require('../../assets/images/companygroup/senbayp dark hover.svg'),
+    },
+    {
+        img1: require('../../assets/images/companygroup/agrobay light.svg'),
+        img2: require('../../assets/images/companygroup/agrobay dark.svg'),
+        img3: require('../../assets/images/companygroup/agrobay light hover.svg'),
+        img4: require('../../assets/images/companygroup/agrobay dark hover.svg'),
+    },
+    {
+        img1: require('../../assets/images/companygroup/turkeli light.svg'),
+        img2: require('../../assets/images/companygroup/turkeli dark.svg'),
+        img3: require('../../assets/images/companygroup/turkeli light hover.svg'),
+        img4: require('../../assets/images/companygroup/turkeli dark hover.svg'),
+    },
+    {
+        img1: require('../../assets/images/companygroup/hasanbey light.svg'),
+        img2: require('../../assets/images/companygroup/hasanbey dark.svg'),
+        img3: require('../../assets/images/companygroup/hasanbey light hover.svg'),
+        img4: require('../../assets/images/companygroup/hasanbey dark hover.svg'),
+    },
+    {
+        img1: require('../../assets/images/companygroup/sut kardesler light.svg'),
+        img2: require('../../assets/images/companygroup/sut kardesler dark.svg'),
+        img3: require('../../assets/images/companygroup/sut kardesler light hover.svg'),
+        img4: require('../../assets/images/companygroup/sut kardesler dark hover.svg'),
+    },
+    {
+        img1: require('../../assets/images/companygroup/ankara-sigorta light.svg'),
+        img2: require('../../assets/images/companygroup/ankara-sigorta dark.svg'),
+        img3: require('../../assets/images/companygroup/ankara-sigorta light hover.svg'),
+        img4: require('../../assets/images/companygroup/ankara-sigorta dark hover.svg'),
+    },
 ]
