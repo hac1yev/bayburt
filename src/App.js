28b2f3807eca07ec286de229169417becf871562
hac1yev <ilkinhaciyev955@gmail.com>
2023-10-10 16:44:33 +0400

    news

--- src/App.js
+++ src/App.js
@@ -3,30 +3,30 @@ import Header from './components/Header/Header';
 import { Routes } from 'react-router-dom';
 import { Route } from 'react-router-dom'; 
 import Footer from './components/Footer/Footer';
-// import Home from './pages/Home';
+import Home from './pages/Home';
 import { useSelector } from 'react-redux';
-// import History from './pages/History/History';
-// import Mission from './pages/Mission/Mission';
-// import Siyaset from './pages/Politics/Siyaset';
+import History from './pages/History/History';
+import Mission from './pages/Mission/Mission';
+import Siyaset from './pages/Politics/Siyaset';
 import DirectorsBoard from './pages/DirectorBoards/DirectorsBoard';
-// import Projects from './pages/Projects/Projects';
-// import Certificat from './pages/Certificate/Certificat';
-// import Founder from './pages/Founder/Founder';
-// import Contact from './pages/Contact/Contact';
-// import Activity from './pages/Activity/Activity';
-// import ProjectDetail from './pages/Projects/ProjectDetail';
-// import HighwayProjects from './pages/Projects/HighwayProjects';
-// import SubwayProjects from './pages/Projects/SubwayProjects';
-// import DamPortProjects from './pages/Projects/DamPortProjects';
-// import AirportProjects from './pages/Projects/AirportProjects';
-// import SuperstructureProjects from './pages/Projects/SuperstructureProjects';
-// import RailwayProjects from './pages/Projects/RailwayProjects';
-// import Awards from './pages/Awards/Awards';
-// import Construction from './pages/Construction/Construction';
-// import CompanyGroups from './pages/CompanyGroups/CompanyGroups';
-// import News from './pages/News/News';
-// import NewsDetail from './pages/News/NewsDetail';
-// import SosialService from './pages/SosialResponsibility/SosialResponsibility';
+import Projects from './pages/Projects/Projects';
+import Certificat from './pages/Certificate/Certificat';
+import Founder from './pages/Founder/Founder';
+import Contact from './pages/Contact/Contact';
+import Activity from './pages/Activity/Activity';
+import ProjectDetail from './pages/Projects/ProjectDetail';
+import HighwayProjects from './pages/Projects/HighwayProjects';
+import SubwayProjects from './pages/Projects/SubwayProjects';
+import DamPortProjects from './pages/Projects/DamPortProjects';
+import AirportProjects from './pages/Projects/AirportProjects';
+import SuperstructureProjects from './pages/Projects/SuperstructureProjects';
+import RailwayProjects from './pages/Projects/RailwayProjects';
+import Awards from './pages/Awards/Awards';
+import Construction from './pages/Construction/Construction';
+import CompanyGroups from './pages/CompanyGroups/CompanyGroups';
+import News from './pages/News/News';
+import NewsDetail from './pages/News/NewsDetail';
+import SosialService from './pages/SosialResponsibility/SosialResponsibility';
 
 const App = () => {
   const mode = useSelector(state => state.modeReducer.mode);
@@ -35,12 +35,12 @@ const App = () => {
     <div className={mode === 'dark' ? 'dark' : ''}>
       <Header />
       <Routes>
-          {/* <Route path='/' element={<Home />} />
+          <Route path='/' element={<Home />} />
           <Route path='/about/history' element={<History />} />
           <Route path='/about/mission' element={<Mission />} />
-          <Route path='/about/politics' element={<Siyaset />} /> */}
+          <Route path='/about/politics' element={<Siyaset />} />
           <Route path='/about/directors-board' element={<DirectorsBoard />} />
-          {/* <Route path='/projects' element={<Projects />} />
+          <Route path='/projects' element={<Projects />} />
           <Route path='/projects/highway' element={<HighwayProjects />} />
           <Route path='/projects/railway' element={<RailwayProjects />} />
           <Route path='/projects/subway' element={<SubwayProjects />} />
@@ -57,7 +57,7 @@ const App = () => {
           <Route path='/about/companyGroups' element={<CompanyGroups />} />
           <Route path='/news' element={<News />} />
           <Route path='/news/:newsId' element={<NewsDetail />} />
-          <Route path='/social-responsibility' element={<SosialService />} /> */}
+          <Route path='/social-responsibility' element={<SosialService />} />
       </Routes>
       <Footer />
     </div>
