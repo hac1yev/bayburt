1b82355775379646f7feef4ba8db372981649f0e
hac1yev <ilkinhaciyev955@gmail.com>
2023-10-13 15:41:56 +0400

    idare heyeti

--- src/pages/Activity/Activity.jsx
+++ src/pages/Activity/Activity.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../../components/Banner/Banner';
 import { activityData } from '../../assets/dummyApi/activity-api';
 import './Activity.css';
+import { Link } from 'react-router-dom';
 
 const Activity = () => {
     return (
@@ -11,13 +12,13 @@ const Activity = () => {
                 <div className="row">
                     {activityData.map((item) => (
                         <div className="col-lg-4 col-md-6 activity-col">
-                            <div className='activity-card'>
+                            <Link onClick={() => window.scrollTo(0,0)} to="/construction" className='activity-card'>
                                 <div className='activity-img'>
                                     <img src={item.img} alt={item.title} />
                                 </div>
                                 <h4>{item.title}</h4>
                                 <p>{item.desc}</p>
-                            </div>
+                            </Link>
                         </div>
                     ))}
                 </div>
